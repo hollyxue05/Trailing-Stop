bb0c2a941a3e44d8ebe06c713e404aeb0caa24e0
holly <hollyxue05@gmail.com>
2025-07-21 23:37:22 -0400

    improved getPeakPrice, cleaned up calculateTrailingStopPrice

--- Trailing Stop/trailingStop.py
+++ Trailing Stop/trailingStop.py
@@ -1,58 +1,40 @@
-import yfinance as yf  # type: ignore
+import yfinance as yf  
 import numpy as np
+import pandas as pd
 from datetime import datetime
 
-# def getPortfolioStockList():
-#     print("temp")
-
 purchaseDate = datetime(2025, 7, 14)
-stockTicker = "CRCL" 
-previousPeakValue = 262.97
-stopPercent = 0.2
-trailingStopPrice = 1
-
-
+stockTicker = "CRCL" #given from the user's spreadsheet or input 
+stopPercent = 0.2    #user inputted
+previousPeakValue = 262.97  #given from the user's spreadsheet or input  
+trailingStopPrice = previousPeakValue * (1-stopPercent)
+    
 def getCurrPrice():
     myTicker = yf.Ticker(stockTicker)
     currPrice = myTicker.fast_info.last_price
-    print(currPrice)
     return currPrice
 
 def getPeakPrice():
-    data = yf.download(stockTicker, start=purchaseDate)
-    max_high = data["High"].max()
-    print(max_high)
-    return max_high
+    data = yf.Ticker(stockTicker).history(start = purchaseDate)
+    max_high = data['High'].max()
+    return float(max_high)
 
 def calculateTrailingStopPrice():
-#      for each stock in the excel file, do
-#     getPeakPrice()
-#     If peak value > excelPeakValue
-#        update peak price to excel 
-#        read "stop%" value, calculate new trailing stop price 
-#        update excel file
-#    fi
-#    if current price < trailing stop price
-#      alert()
-#    fi
-#   done
+    currPeakPrice = getPeakPrice() 
+    currPrice = getCurrPrice() 
 
-    # trailingStopPrice = 210.376
-
-    currPeakPrice = getPeakPrice()
-    currPrice = getCurrPrice()
-
-    if int(currPeakPrice) > int(previousPeakValue) : 
-        print("got here")
+    # currPeakPrice is ambiguous rn 
+    if currPeakPrice > previousPeakValue : 
         #update excel peak price 
         global trailingStopPrice 
         trailingStopPrice = (1-stopPercent) * currPeakPrice  
         # update excel 
-
-    print(trailingStopPrice)
     
     if int(currPrice) < int(trailingStopPrice) : 
         alertCell(currPrice) 
+    else :
+        print("current trailing stop =", trailingStopPrice)
+        print("current price = ", currPrice)
 
 
 def alertCell(price):
